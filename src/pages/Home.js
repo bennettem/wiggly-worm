b6fac60813ed7f5b59eeded3794bc0d50f705da7
bennettem <emily.pamelabennett@gmail.com>
2022-07-18 23:58:32 -0500

    added project images/projectItem/projects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function Home () {
     return (
-        <div>Home</div>
+        <div className="home">
+            <div className="about">
+                <h2>Hi, My Name is Emily</h2>
+                <div className="aboutInfo">
+                    <p>
+                        A software developer in training with a passion for learning and creating.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,23 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import codingNkolbolds from "../assets/codingNkolbolds.jpg";
+import dice from "../assets/dice.png";
+import number from "../assets/number.png";
+import robot from "../assets/robot.png";
+import where2 from "../assets/where2.png";
 
 function Projects () {
     return (
-        <div>Project</div>
+        <div className="projects">
+            <h1>My Projects</h1>
+            <div className="projectList">
+                <ProjectItem name="Coding N Kolbolds" image={codingNkolbolds}/>
+                <ProjectItem name="Dice Game" image={dice}/>
+                <ProjectItem name="Number Game" image={number}/>
+                <ProjectItem name="Robot Gladiator" image={robot}/>
+                <ProjectItem name="Where2 Travel" image={where2}/>
+            </div>
+        </div>
     );
 }
 
